a4a22336272efed2a3028ec5c2f1667bf90df5d1
eraldo06 <eraldoleonardo384@gmail.com>
2022-08-10 20:43:57 -0300

    1 projeto

---
+++ 1projeto/database.js
@@ -0,0 +1,58 @@
+const livros= [
+    {
+        id: 1,
+        nome: 'digital minumalista',
+        autor: 'Cal newport',
+        categorria: 'Produto e estilo de vida',
+        paginas: 254,
+        recomendo: false,
+        leu: false
+    },
+    {
+        id: 2,
+        nome: 'digital minumalista',
+        autor: 'Cal newport',
+        categorria: 'Tecnologia',
+        paginas: 254,
+        recomendo: false,
+        leu: false
+    },
+    {
+        id: 3,
+        nome: 'digital minumalista',
+        autor: 'Cal newport',
+        categorria: 'Produto e estilo de vida',
+        paginas: 254,
+        recomendo: false,
+        leu: false
+    },
+    {
+        id: 4,
+        nome: 'digital minumalista',
+        autor: 'Cal newport',
+        categorria: 'Produto e estilo de vida',
+        paginas: 254,
+        recomendo: false,
+        leu: false
+    },
+    {
+        id: 5,
+        nome: 'digital minumalista',
+        autor: 'Cal newport',
+        categorria: 'Produto e estilo de vida',
+        paginas: 254,
+        recomendo: false,
+        leu: false
+    },
+    {
+        id: 6,
+        nome: 'digital minumalista',
+        autor: 'Cal newport',
+        categorria: 'Produto e estilo de vida',
+        paginas: 254,
+        recomendo: false,
+        leu: false
+    }
+]
+
+module.exports = livros //exportar
