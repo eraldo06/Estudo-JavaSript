755111636ec8cb5d123c0cfb52b37552ba605d80
eraldo06 <eraldoleonardo384@gmail.com>
2022-08-11 20:05:49 -0300

    apredendo a como modificar um link no href

--- aulas-dom/modificação/index.js
+++ aulas-dom/modificação/index.js
@@ -5,4 +5,11 @@ let umSpan = document.createElement("span"); // foi criado um elemento
 let textoSpan = document.createTextNode("Nosso span está pegando ?") // foi criado o conteudo do elemente novo
 umSpan.appendChild(textoSpan); // junção do novo elemento com o conteudo
 
+
 paiDoElementoP.replaceChild(umSpan, elementoP) // o p foi substituído pelo o novo elemento
+
+
+// setAttribute
+
+let linkYT = document.querySelector("a"); // está adicionando o a na variavel
+linkYT.setAttribute("href","https://www.youtube.com/watch?v=srhVCrcx2uw/"); // está modificando o link do href
