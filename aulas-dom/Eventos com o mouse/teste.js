4b88d1b34adec5f3653a8ead84197308bf405a8f
eraldo06 <eraldoleonardo384@gmail.com>
2022-08-16 18:57:12 -0300

    eventos com o mouse

--- aulas-dom/Eventos com o mouse/teste.js
+++ aulas-dom/Eventos com o mouse/teste.js
@@ -3,21 +3,21 @@
 
 
 
-var buttonT = document.getElementById("action-btn-t");
+var buttonT = document.getElementById("action-btn-t");// botão que vai chamar
 
 buttonT.addEventListener("click", function() {
 
-var containerB  = document.getElementById("container-css");
-containerB.classList.toggle("hide");    
+var containerB  = document.getElementById("container-css");// conteudo que vai modificar
+containerB.classList.toggle("hide");    // class que vai adicionar
 });
 
 
 // fechar o formulario
 
-var buttonT = document.getElementById("butt");
+var buttonT = document.getElementById("butt");// botão que vai chamar
 
 buttonT.addEventListener("click", function() {
 
-var containerB  = document.getElementById("container-css");
-containerB.classList.toggle("hide");    
+var containerB  = document.getElementById("container-css");// conteudo que vai modificar
+containerB.classList.toggle("hide");    // class que vai adicionar
 });
