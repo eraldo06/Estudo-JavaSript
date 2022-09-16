4cf5845eefc8af10f8aaac9485248448fc0420e4
eraldo06 <eraldoleonardo384@gmail.com>
2022-09-16 20:46:41 -0300

    function 03

---
+++ Introdução/Functions/function02.js
@@ -0,0 +1,22 @@
+// function expression
+//  function anonymous
+
+// const sum = function(n1, n2){
+//     console.log(n1+n2);
+// }
+
+// sum(2,3)
+
+/*--------------------------------------------------------*/
+
+const sum = function(n1,n2){
+    total = n1+n2
+    return total
+}
+
+let n1 = 34;
+let n2 = 65;
+
+console.log(`o numero 1 é ${n1}`);
+console.log(`o numero 2 é ${n2}`);
+console.log(`a soma é ${sum(n1,n2)}`);
