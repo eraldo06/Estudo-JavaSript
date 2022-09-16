5259ad5de19111951e0178d278257bdf6bfc35b7
eraldo06 <eraldoleonardo384@gmail.com>
2022-09-16 19:36:13 -0300

    function 01

---
+++ Introdução/Functions/js.js
@@ -0,0 +1,10 @@
+// função
+function createPhrases(){
+    console.log('Estudar é muito bom');
+console.log('Paciência e persistência');
+console.log('Revisão é mãe do aprendizado');
+}
+
+
+// chamando a função
+createPhrases()
