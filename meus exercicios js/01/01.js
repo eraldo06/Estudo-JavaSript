1167862d5f0e426170c6ae4ee7544060bb5de97d
eraldo06 <eraldoleonardo384@gmail.com>
2022-09-16 19:07:37 -0300

    Exercicios 01

---
+++ meus exercicios js/01/01.js
@@ -0,0 +1,21 @@
+const pessoa= {
+    nome: 'eraldo',
+    age: 20,
+    starts: 2.5,
+    isSubscribed: true,
+}
+
+console.log(pessoa);
+
+
+
+console.log(`${pessoa.nome} tem ${pessoa.age} anos`);
+// resposta esperada 'eraldo tem 20 anos'
+
+
+// colocando um objeto no array
+let pesssoas=[
+    pessoa
+]
+
+console.log(pesssoas);
