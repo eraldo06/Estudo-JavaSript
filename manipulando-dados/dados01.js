6014de2574d2163d7978e4dc963666c9e05ca14e
eraldo06 <eraldoleonardo384@gmail.com>
2022-09-18 11:41:44 -0300

    transformando dados

---
+++ manipulando-dados/dados01.js
@@ -0,0 +1,6 @@
+// Transformar String em numeros e Numero em String
+
+let string = "123";
+console.log(Number(string)); // isso vira numero
+let number = 123;
+console.log(String(number));//isso vira string
