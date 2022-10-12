4fba5e6e8a5a71138a4f6e4c68df2e5bb7e610cd
eraldo06 <eraldoleonardo384@gmail.com>
2022-10-12 09:54:37 -0300

    API

--- API/Aula01/index.js
+++ API/Aula01/index.js
@@ -1,10 +1,36 @@
-const url = "http://localhost:5500/api"
+const url = "http://localhost:5500/api" // pegando a url da api
 
-function getUsuario(){
+
+// pegando todos os usuários
+function getUsuarios(){
     fetch(url)
     .then(res => (res.json()))
-    .then(data => renderApiResult.textContent = JSON.stringify(data))
+    .then(data => renderApiResult2.textContent = JSON.stringify(data))
     .catch(error => console.log(error))
 }
+getUsuarios()
 
+
+// pegando só um usuário
+function getUsuario(){
+    fetch(`${url}/1`)
+    .then(res => (res.json()))
+    .then(data => { 
+        return renderApiResult.textContent = JSON.stringify(data)})
+    .catch(error => console.log(error))
+}
 getUsuario()
+
+// --------------------------------------------
+
+function getUser(){
+    fetch(`${url}/1`)
+    .then(res => (res.json()))
+    .then(data => { 
+        userName.textContent = data.name
+        userCity.textContent = data.city
+        userAvatar.src = data.avatar
+    })
+    .catch(error => console.log(error))
+}
+getUser()
