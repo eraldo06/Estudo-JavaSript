132b2372dc90679fb4d491529f948b32b57c9ae2
eraldo06 <eraldoleonardo384@gmail.com>
2022-10-11 22:09:31 -0300

    api

--- API/Aula01/index.js
+++ API/Aula01/index.js
@@ -1,1 +1,10 @@
 const url = "http://localhost:5500/api"
+
+function getUsuario(){
+    fetch(url)
+    .then(res => (res.json()))
+    .then(data => renderApiResult.textContent = JSON.stringify(data))
+    .catch(error => console.log(error))
+}
+
+getUsuario()
