5ef61de9a8509124e6a153f540b495f0b7a30acd
eraldo06 <eraldoleonardo384@gmail.com>
2022-10-27 20:58:38 -0300

    fazendo o carregamento dos posts

---
+++ Exercicio Fetch Api/js/scripts.js
@@ -0,0 +1,39 @@
+const url = "https://jsonplaceholder.typicode.com/posts";
+// pegando a api pela url
+
+const loadingElement = document.querySelector('#loading') // pegando html
+const postsContainer = document.querySelector("#posts-container") // pegando html
+
+// Pegando todos os pots
+
+
+// função async await vai rodar uma linha por vez, 
+// assim dá mais seguraça no cod, 
+async function getAllPosts(){
+    const resp = await fetch(url)
+    const data  = await resp.json();
+    console.log(data);
+
+    // quando acabar de cosumir as apis, apague o "loadingElement"
+    loadingElement.classList.add("hide");
+
+    data.map((post)=>{
+        const div = document.createElement("div");
+        const title = document.createElement("h2");
+        const body = document.createElement("p");
+        const link = document.createElement("a");
+
+        title.innerText = post.title;
+        body.innerText = post.body;
+        link.innerText = "ler";
+        link.setAttribute("href", `/post.html?id=${post.id}`)
+
+        div.appendChild(title);
+        div.appendChild(body);
+        div.appendChild(link);
+
+        postsContainer.appendChild(div);
+    });
+}
+
+getAllPosts()
