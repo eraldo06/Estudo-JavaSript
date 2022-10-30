1ea816736d828d8afccb48567b6bd5b7e2481d34
eraldo06 <eraldoleonardo384@gmail.com>
2022-10-30 15:56:11 -0300

    FETCH

--- aulas-B7Web/aulaFetch/index.js
+++ aulas-B7Web/aulaFetch/index.js
@@ -1,19 +1,23 @@
+
 function loadPosts(){
-    
+    document.getElementById('posts').innerHTML = "Carregando"    
+
+    // pegando url e transformando em json
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then(function(resul){
+            console.log(resul);
             return resul.json()
         })
+
+        // colocando o resulto no HTML
         .then(function(resul){
             document.getElementById('posts').innerHTML = resul.length+ " posts";
         })
+
+        // tratramento de erro
         .catch(function(errorr){
             console.log("Deu erro");
             document.getElementById('posts').innerHTML = "deu erro"    
         })
-
-    document.getElementById('posts').innerHTML = "Carregando"    
- 
+    }
    
-    console.log("vim primeiro");
-}
